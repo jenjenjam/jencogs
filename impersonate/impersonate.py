333eb17e2114feca934a03b0b763165bf8996d5a
Jam <>
2020-05-21 18:15:28 +0100

    Update to use data download

--- impersonate/impersonate.py
+++ impersonate/impersonate.py
@@ -16,7 +16,7 @@ class Impersonate(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, 573147218420571)
+        self.config = Config.get_conf(self, 573147218420573)
         self.bot_prefixes = ['&', '-', '.', '#', '\'']
         self.config.register_user(
             markov=None,
@@ -26,39 +26,39 @@ class Impersonate(commands.Cog):
 
         self.config.register_guild(
             tries=200, 
-            max_overlap_ratio=0.8, 
+            max_overlap_ratio=0.7, 
             max_overlap_total=20,
-            message_limit=None,
-            ignore_channels=[]
+            history={}
+        )
+
+        self.config.register_channel(
+            channel_download_date=None
         )
 
     async def save_model(self, model, user, text_size):
-        await self.config.user(user).markov_date.set(
-                datetime.timestamp(datetime.now())
-            )
         await self.config.user(user).markov.set(
                 model.to_json()
             )
         await self.config.user(user).markov_size.set(
                 text_size
             )
+        await self.config.user(user).markov_date.set(
+                datetime.timestamp(datetime.now())
+            )
 
-    async def generate_new_model(self, ctx, user, m, limit=None, date=None):
+    async def generate_new_model(self, ctx, user, m):
         channels = ctx.guild.text_channels
-        ignore_channels = await self.config.guild(ctx.message.guild).ignore_channels()
+        cfg = self.config.guild(ctx.message.guild)
+        history = await cfg.history()
+        print(user.id)
+        print(list(history.keys()))
+        if str(user.id) not in list(history.keys()):
+            await m.edit(content=f"No data for this user")
+            return None, 0
 
-        history = []
-        for c in channels:
-            if c.id not in ignore_channels:
-                try:
-                    async for msg in c.history(limit=limit, after=date):
-                        if msg.author == user and msg.clean_content[0] not in self.bot_prefixes:
-                            history.append(msg.clean_content)
-                except:
-                    pass
-
+        history = history[ str(user.id)]
         if len(history) == 0:
-            await m.edit(content=f"No new messages...")
+            await m.edit(content=f"No messages...")
             return None, 0
         
         await m.edit(content=f"Generating model with {len(history)} messages from {user}...")
@@ -79,11 +79,53 @@ class Impersonate(commands.Cog):
         pass
 
     @commands.guild_only()
+    @checks.admin()
+    @impersonate.command()
+    async def getdata(self, ctx, channel: discord.TextChannel = None, limit: int = 5000):
+        cfg = self.config.guild(ctx.message.guild)
+        history = await cfg.history()
+
+        if channel is None:
+            channel = ctx.message.channel
+
+        from_date = await self.config.channel(channel).channel_download_date()
+        if not from_date is None:
+            m = await ctx.send(f"Attempting to load up to {limit} messages from after date {from_date} in channel {channel}")
+            from_date = datetime.fromtimestamp(from_date)
+        else:
+            m = await ctx.send(f"Attempting to load up to {limit} messages in channel {channel}")
+
+        msg_counter = 0
+        async for msg in channel.history(limit=limit, after=from_date):
+            if not msg.author.bot and len(msg.clean_content) > 0 and msg.clean_content[0] not in self.bot_prefixes:
+                if str(msg.author.id) not in list(history.keys()):
+                    history[str(msg.author.id)] = []
+                history[str(msg.author.id)].append(msg.clean_content)
+                msg_counter += 1
+        
+        print(history)
+        await cfg.history.set(
+                history
+            )
+        await  self.config.channel(channel).channel_download_date.set(
+                datetime.timestamp(datetime.now())
+            )
+        await ctx.send(f"Saved {msg_counter} messages")
+
+    @commands.guild_only()
+    @checks.admin()
+    @impersonate.command()
+    async def cleardata(self, ctx):
+        await self.config.guild(ctx.message.guild).history.set(
+                {}
+            )
+        await self.config.clear_all_channels()
+        await ctx.send(f"Cleared all data")
+
+    @commands.guild_only()
     @impersonate.command()
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def generate(self, ctx, user: discord.User = None, force_redo: bool = False, sentences: int = 1):
-        if not ctx.invoked_subcommand is None:
-            return
         if user is None:
             user = ctx.message.author
         if sentences <= 0 or sentences >= 10:
@@ -91,27 +133,28 @@ class Impersonate(commands.Cog):
             return
         
         cfg = self.config.guild(ctx.message.guild)
-        limit = await cfg.message_limit()
         
         date = await self.config.user(user).markov_date()
         if force_redo or date is None:
             m = await ctx.send(f"Generating model for {user}...")
             # Make new model
-            model, size = await self.generate_new_model(ctx, user, m, limit=limit)
+            model, size = await self.generate_new_model(ctx, user, m)
+            if size == 0:
+                return
             await self.save_model(model, user, size)
 
-        elif datetime.now() - datetime.fromtimestamp(date) > timedelta(hours=24):
-            # Update model
-            m = await ctx.send(f"Updating model for {user}...")
-            new_model, new_size = await self.generate_new_model(ctx, user, m, datetime.fromtimestamp(date), limit=limit)
-            old_model = markovify.Text.from_json(await self.config.user(user).markov())
-            old_size = await self.config.user(user).markov_size()
-            if new_size == 0:
-                model = old_model
-            else:
-                model = markovify.combine([ old_model, new_model ], [ int(old_size), int(new_size) ])
-                await self.save_model(model, user, old_size + new_size)
-                await m.edit(content=f"Model updated and saved for {user}")
+        # elif datetime.now() - datetime.fromtimestamp(date) > timedelta(hours=24):
+        #     # Update model
+        #     m = await ctx.send(f"Updating model for {user}...")
+        #     new_model, new_size = await self.generate_new_model(ctx, user, m, datetime.fromtimestamp(date), limit=limit)
+        #     old_model = markovify.Text.from_json(await self.config.user(user).markov())
+        #     old_size = await self.config.user(user).markov_size()
+        #     if new_size == 0:
+        #         model = old_model
+        #     else:
+        #         model = markovify.combine([ old_model, new_model ], [ int(old_size), int(new_size) ])
+        #         await self.save_model(model, user, old_size + new_size)
+        #         await m.edit(content=f"Model updated and saved for {user}")
 
         else:
             # Reuse old model
@@ -132,31 +175,4 @@ class Impersonate(commands.Cog):
         s = f"{user}: {'. '.join(preds)}." 
 
         await ctx.send(s)
-
-    @commands.guild_only()
-    @checks.admin()
-    @impersonate.command()
-    async def ignorechannel(self, ctx, channel: discord.TextChannel):
-        ignore_channels = await self.config.guild(ctx.message.guild).ignore_channels()
-        if channel.id in ignore_channels:
-            ignore_channels.remove(channel.id)
-            await self.config.guild(ctx.message.guild).ignore_channels.set(
-                ignore_channels
-            )
-            await ctx.send(f"Channel {channel} removed from ignored list")
-        else:
-            ignore_channels.append(channel.id)
-            await self.config.guild(ctx.message.guild).ignore_channels.set(
-                ignore_channels
-            )
-            await ctx.send(f"Channel {channel} added to ignored list")
-
-    @commands.guild_only()
-    @checks.admin()
-    @impersonate.command()
-    async def setmessagelimit(self, ctx, limit: int = None):
-        await self.config.guild(ctx.message.guild).message_limit.set(
-            limit
-        )
-        await ctx.send(f"Message limit set to {limit}")
        
